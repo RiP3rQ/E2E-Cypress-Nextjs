99f35ac63635d3407ef685dce62154b704a24e8c
RiP3rQ <rafalpompa2000@gmail.com>
2023-12-27 22:16:00 +0100

    Basic Components Testing via Cypress - Setup

--- cypress.config.js
+++ cypress.config.js
@@ -8,4 +8,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
 });
---
+++ cypress/component/Accordion.cy.jsx
@@ -0,0 +1,5 @@
+describe("Accordion.cy.jsx", () => {
+  it("playground", () => {
+    // cy.mount()
+  });
+});
